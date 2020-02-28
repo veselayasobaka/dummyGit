b46264b64281a3b95f6d45f32734a3361c1cf7f0
veselayasobaka <a.marin30692@gmail.com>
2020-02-28 16:28:32 +0300

    modified main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+extern int func();
+
 // this is just a dummy project to test git commands
 
 using std::cout;
@@ -7,7 +9,7 @@ using std::endl;
 
 int main()
 {
-    cout << "Hello World" << endl;
+    cout << "Hello World" << func() << endl;
     
     return 0;
 }
